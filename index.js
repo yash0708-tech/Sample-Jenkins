7117a82b97c4805ee4c977ce7f9cd6d35cc69f16
yash0708-tech <155578612+yash0708-tech@users.noreply.github.com>
2024-02-02 07:01:19 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express=require('express');
 const app=express();
 
 app.get('/',(req,res)=>{
-    res.send("Hello World");
+    res.send("Hellooooo World");
 })
 
 app.listen(8000,()=>console.log("Server is up and running"));
